32333547b56eb4d02ad0b0db1ebaec16470ff68e
Boris Popik <popik.boris@gmail.com>
2019-03-05 14:21:28 +0000

    L

--- Shortify/Program.cs
+++ Shortify/Program.cs
@@ -7,13 +7,28 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Shortify.Models;
 
 namespace Shortify
 {
     public class Program
     {
+        static void TestStuff()
+        {
+            var tree = new WordTrie<string>("root");
+            tree.AddChild("asd");
+            tree.AddChild("dasd");
+            var a = tree.Flatten();
+            foreach (var x in a)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+
         public static void Main(string[] args)
         {
+            TestStuff();
             CreateWebHostBuilder(args).Build().Run();
         }
 
